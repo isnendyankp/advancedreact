fe5818ad37d7fe98f3997ec50f58ad2b7cda0d91
isnendyan <isnendyankp@gmail.com>
2023-04-28 10:24:03 +0700

    learn Import for useRef-basic & useState-array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Starter from './tutorial/07-useRef/starter/01-useRef-basics';
+import Final from './tutorial/01-useState/final/03-useState-array';
+
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
+    <div className="container">
+      <Starter />
+      <Final />
     </div>
   );
 }
