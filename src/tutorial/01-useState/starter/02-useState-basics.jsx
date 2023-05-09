38950d144679827a0e7d0242b12062810f382164
isnendyan <isnendyankp@gmail.com>
2023-05-09 07:36:09 +0700

    UseState Fundamentals: create array structure with function update value if click the button

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -2,12 +2,25 @@ import { useState } from "react";
 
 
 const UseStateBasics = () => {
-  const value = useState('hello')[0];
-  const func =  useState('hello')[1];
-  console.log(value);
-  console.log(func);
+  // const value = useState('hello')[0];
+  // const func =  useState('hello')[1];
+  // console.log(value);
+  // console.log(func);
 
-  return <h2>useState basics</h2>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h4>You clicked {count} times</h4>
+      <button type='button' className="btn" onClick={handleClick}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
