a5533ffd0edb439f5f44f62e0c86fdcae9b04017
isnendyan <isnendyankp@gmail.com>
2023-05-09 07:00:30 +0700

    Error Example: learn error example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Starter from './tutorial/07-useRef/starter/01-useRef-basics';
-import Final from './tutorial/01-useState/final/03-useState-array';
+import Starter from './tutorial/01-useState/starter/01-error-example';
+
 
 
 function App() {
   return (
     <div className="container">
       <Starter />
-      <Final />
     </div>
   );
 }
