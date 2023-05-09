0f7d7babbe0b295c38d87ae3071a5aa9dc98d6c2
isnendyan <isnendyankp@gmail.com>
2023-05-09 07:27:24 +0700

    UseState Fundamentals: create 2 items with same value

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 
 
 const UseStateBasics = () => {
-  console.log(useState());
+  const value = useState('hello')[0];
+  const func =  useState('hello')[1];
+  console.log(value);
+  console.log(func);
+
   return <h2>useState basics</h2>;
 };
 
