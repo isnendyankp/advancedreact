d2b2980cce960f82fb763ac96a7119a60aae391b
isnendyan <isnendyankp@gmail.com>
2023-05-10 11:26:40 +0700

    UseState Array Example: Import final component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Starter from "./tutorial/01-useState/starter/03-useState-array.jsx";
+import Final from "./tutorial/01-useState/final/03-useState-array.jsx";
 
 function App() {
   return (
     <div className="container">
       <Starter />
+      <Final />
     </div>
   );
 }
