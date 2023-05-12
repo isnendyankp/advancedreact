e5189ca078db20ecc1d561b878371abc7a2bae57
isnendyan <isnendyankp@gmail.com>
2023-05-12 10:31:22 +0700

    useState Object Example: activating render value

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -4,7 +4,13 @@ const UseStateObject = () => {
   const [name, setName] = useState("peter");
   const [age, setAge] = useState(24);
   const [hobby, setHobby] = useState("read books");
-  return <h2>useState object example</h2>;
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoys To: {hobby}</h4>
+    </>
+  );
 };
 
 export default UseStateObject;
