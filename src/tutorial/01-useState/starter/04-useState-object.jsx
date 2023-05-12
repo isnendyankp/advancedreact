5a7ebf91bfe4116f17a280c737df3e78928c5971
isnendyan <isnendyankp@gmail.com>
2023-05-12 10:34:33 +0700

    useState Object Example: add button with function displayperson base

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -4,11 +4,16 @@ const UseStateObject = () => {
   const [name, setName] = useState("peter");
   const [age, setAge] = useState(24);
   const [hobby, setHobby] = useState("read books");
+
+  const displayPerson = () => {
+  };
+
   return (
     <>
       <h3>{name}</h3>
       <h3>{age}</h3>
       <h4>Enjoys To: {hobby}</h4>
+      <button className='btn' onClick={displayPerson}></button>
     </>
   );
 };
