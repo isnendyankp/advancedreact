9fbf927833595b2a90085cd37d33845412afe848
isnendyan <isnendyankp@gmail.com>
2023-05-12 10:28:15 +0700

    useState Object Example: create state value

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState("read books");
   return <h2>useState object example</h2>;
 };
 
