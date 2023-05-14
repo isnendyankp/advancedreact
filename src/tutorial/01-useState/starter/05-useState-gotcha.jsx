ecba82841a371c285cedceb74ae44a2f19b5c5e2
isnendyan <isnendyankp@gmail.com>
2023-05-14 09:22:12 +0700

    useState Set Function 'gotcha': activating gotcha function

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue(value + 1);
+    //  be careful it's the old value
+    console.log(value);
+    //  so if you have any functionality
+    // that relies on the latest value
+    // it will be wrong !!!
+  };
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button className="btn" onClick={handleClick}>
+        increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
