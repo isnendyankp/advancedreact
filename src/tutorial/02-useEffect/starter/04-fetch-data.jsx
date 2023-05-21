102d7e05afe808f24e06191db68cc27ef8d34678
isnendyan <isnendyankp@gmail.com>
2023-05-21 09:26:44 +0700

    useEffect-Fetch: add useState

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,9 @@
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
+
+const [users, setUsers] = useState([]);
+
   return <h2>fetch data example</h2>;
 };
 export default FetchData;
