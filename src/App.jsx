a9eb47543613334fad95864bd71220d86cd120c7
isnendyan <isnendyankp@gmail.com>
2023-05-22 10:31:27 +0700

    useEffect-clean  Up: beginning setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/04-fetch-data.jsx';
+import Starter from './tutorial/02-useEffect/starter/05-cleanup-function.jsx';
 import Final from './tutorial/01-useState/final/03-useState-array.jsx';
 
 function App() {
