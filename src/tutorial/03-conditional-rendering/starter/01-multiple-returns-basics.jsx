7a88feff144d1fc4c79e0784e8d06baf1c569c1b
isnendyan <isnendyankp@gmail.com>
2023-05-23 11:18:52 +0700

    Mutiple Return: setup useState as a boolean

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+
+
   return <h2>Multiple Returns Basics</h2>;
 };
 export default MultipleReturnsBasics;
