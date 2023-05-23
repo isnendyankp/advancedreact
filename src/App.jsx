cc89d5c3763786e1eb940b7da54173b2b3781c0b
isnendyan <isnendyankp@gmail.com>
2023-05-23 11:11:17 +0700

    Mutiple Return: beginning startup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function.jsx';
+import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx';
 import Final from './tutorial/01-useState/final/03-useState-array.jsx';
 
 function App() {
