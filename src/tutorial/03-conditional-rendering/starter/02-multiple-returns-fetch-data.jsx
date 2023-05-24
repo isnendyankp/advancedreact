97c68916aa796a3286ebdd5f73f7d8cc2002c7fc
isnendyan <isnendyankp@gmail.com>
2023-05-24 10:03:02 +0700

    Mutiple Return: create state value

--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState(null)
+
+  
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;
