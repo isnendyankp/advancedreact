b3844ecf1f2767d26c706e35266b0eeb9232b89f
isnendyan <isnendyankp@gmail.com>
2023-05-24 10:00:43 +0700

    Mutiple Return: beginning setup fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx';
+import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx';
 import Final from './tutorial/01-useState/final/03-useState-array.jsx';
 
 function App() {
