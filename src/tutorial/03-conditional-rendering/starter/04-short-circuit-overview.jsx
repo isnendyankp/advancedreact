6ca90c3fb0c5bac805a5e4d58252cd0468d5a135
isnendyan <isnendyankp@gmail.com>
2023-05-29 11:13:34 +0700

    Section 6-106: navigate setup short circuit-overview

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
 
+/*Setup Challenge :
+
+create two state values
+one "falsy" and second "truthy"
+setup both conditions for each operator in JSX - hint {}
+|| OR
+&& AND*/
+
 const ShortCircuitOverview = () => {
   return <h2>short circuit overview</h2>;
 };
