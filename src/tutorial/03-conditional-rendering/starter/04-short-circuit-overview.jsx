597fb3f2e7d878ba4a909e9737238c9e8dd87763
isnendyan <isnendyankp@gmail.com>
2023-05-29 11:15:32 +0700

    Section 6-106: Create two state values one(falsy) & second (truthy)

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -9,6 +9,11 @@ setup both conditions for each operator in JSX - hint {}
 && AND*/
 
 const ShortCircuitOverview = () => {
+  // falsy
+  const [text, setText] = useState('');
+  // truthy
+  const [name, setName] = useState('susan');
+
   return <h2>short circuit overview</h2>;
 };
 export default ShortCircuitOverview;
