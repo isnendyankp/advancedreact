5935b9fdb20c688273822d0ea19b382c204c3c35
isnendyan <isnendyankp@gmail.com>
2023-05-29 11:21:39 +0700

    Section 6-106:setup both conditions for each operator in JSX

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,19 +1,24 @@
 import { useState } from 'react';
 
-/*Setup Challenge :
-
-create two state values
-one "falsy" and second "truthy"
-setup both conditions for each operator in JSX - hint {}
-|| OR
-&& AND*/
-
 const ShortCircuitOverview = () => {
   // falsy
   const [text, setText] = useState('');
   // truthy
   const [name, setName] = useState('susan');
 
-  return <h2>short circuit overview</h2>;
+  const codeExample = text || 'hello world';
+
+  // can't use if statements
+  return (
+    <div>
+      {/* {if(someCondition){"won't work"}} */}
+
+      <h4>Falsy OR : {text || 'hello world'}</h4>
+      <h4>Falsy AND {text && 'hello world'}</h4>
+      <h4>Truthy OR {name || 'hello world'}</h4>
+      <h4>Truthy AND {name && 'hello world'}</h4>
+      {codeExample}
+    </div>
+  );
 };
 export default ShortCircuitOverview;
