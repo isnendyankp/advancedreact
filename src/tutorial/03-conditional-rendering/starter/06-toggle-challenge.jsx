8e4b0433f1abf970de17b26538450e1e772ff316
isnendyan <isnendyankp@gmail.com>
2023-05-31 11:45:30 +0700

    section 6-109-Toggle Challenge: Create state value & btn toogle

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 /* 
 -create state value (boolean)
 -return a button and a component/element
 -when user clicks the button:
 *toggle state value
-*conditionally render component/element*/ 
+*conditionally render component/element*/
 
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setShowAlert] = useState(false);
+  return (
+    <div>
+      <button className="btn">toogle</button>
+    </div>
+  );
 };
 
 export default ToggleChallenge;
