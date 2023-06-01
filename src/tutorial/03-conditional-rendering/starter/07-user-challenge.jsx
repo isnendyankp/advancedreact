e061aabbb37f61b512fa84111cb7c7c588d2509d
isnendyan <isnendyankp@gmail.com>
2023-06-01 10:10:25 +0700

    Section 6-110: setup user challenge

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,3 +1,15 @@
+// create state value:1. user - default value null
+//create two functions
+
+/*-login - set's user equal to object with name property
+  -logout - set's user equal to null*/
+// in jsx use ? to display two different setups
+
+// h4 with "hello there, user name" and logout button
+
+// h4 with "please login " and login button
+import { useState } from 'react';
+
 const UserChallenge = () => {
   return <h2>user challenge</h2>;
 };
