ff4d7c39951253fed38f52b7543cc908ad742408
isnendyan <isnendyankp@gmail.com>
2023-06-02 10:26:41 +0700

    Section 6-113:there's no new interval if every time rerender

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -25,11 +25,15 @@ const CleanupFunction = () => {
 };
 const RandomComponent = () => {
   useEffect(() => {
-    console.log('hmm, this is interesting');
-    setInterval(() => {
+    // console.log('hmm, this is interesting');
+    const intID = setInterval(() => {
       console.log('hello from interval');
     }, 1000);
+    // does not stop, keeps going
+    // every time we render component new interval gets created
+    return () => clearInterval(intID);
   }, []);
+
   return <h1>hello there</h1>;
 };
 export default CleanupFunction;
