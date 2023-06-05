26624bd7d6f108d1313636ff371345def2784570
isnendyan <isnendyankp@gmail.com>
2023-06-05 11:12:01 +0700

    S 6-116: Import Navbar on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function.jsx';
-import Final from './tutorial/01-useState/final/03-useState-array.jsx';
+import Navbar from "./tutorial/04-project-structure/starter/Navbar/Navbar";
+
 
 function App() {
   return (
     <div className="container">
-      <Starter />
-      {/* <Final /> */}
+      <Navbar />
     </div>
   );
 }
