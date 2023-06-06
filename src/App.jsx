2aedd64bdb9e9407cdd6d7f87fb091096434b593
isnendyan <isnendyankp@gmail.com>
2023-06-06 10:22:55 +0700

    S 6-116: Export to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navbar from "./tutorial/04-project-structure/starter/Navbar";
+import About from "./tutorial/04-project-structure/starter/Pages/About";
+import Home from "./tutorial/04-project-structure/starter/Pages/Home";
 
 
 function App() {
   return (
     <div className="container">
-      <Navbar />
+      <Home />
+      <About />
     </div>
   );
 }
