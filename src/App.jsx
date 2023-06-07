0c680b1a2307511736b5d438c4381a6320fbacc5
isnendyan <isnendyankp@gmail.com>
2023-06-07 11:48:53 +0700

    S 6-118: Import example on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Example from "./tutorial/04-project-structure/starter/Example";
 
 
 
 function App() {
   return (
     <div className="container">
-      
+      <Example />
     </div>
   );
 }
