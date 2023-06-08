5cee4be5905671b52fc14569f34c1409acca53fe
isnendyan <isnendyankp@gmail.com>
2023-06-08 07:51:41 +0700

    S 6-120: navigate with import list on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import List from "./tutorial/05-leverage-javascript/starter/List";
 
 
 function App() {
   return (
     <div className="container">
       <h2>Leverage Javascript</h2>
-      
+      <List />
     </div>
   );
 }
