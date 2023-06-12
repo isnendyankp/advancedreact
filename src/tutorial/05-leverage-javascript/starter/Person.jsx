de5379d6e1b318e68694840a9265cc35341584b6
isnendyan <isnendyankp@gmail.com>
2023-06-12 10:44:45 +0700

    S 6-121: small property if first one exist

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 export function Person ({ name, nickName = 'shakeAndBake', images }) {
-
+  // if the images exist, then look for the first one
+  // if the first one exist, then get me the small property
+  const img = images && images[0] && images [0].small && images [0].small.url;
   return (
     <div>
       <h4>{name}</h4>
