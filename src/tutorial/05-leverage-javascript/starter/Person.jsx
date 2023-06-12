720bfd486d2afd1f3ff2bed50073d6015f6a8ede
isnendyan <isnendyankp@gmail.com>
2023-06-12 10:45:49 +0700

    S 6-121: add img src with style

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -6,6 +6,7 @@ export function Person ({ name, nickName = 'shakeAndBake', images }) {
   const img = images && images[0] && images [0].small && images [0].small.url;
   return (
     <div>
+      <img src={img} alt={name} style={{ width: '50px' }} />
       <h4>{name}</h4>
       <p>Nickname : {nickName}</p>
     </div>
