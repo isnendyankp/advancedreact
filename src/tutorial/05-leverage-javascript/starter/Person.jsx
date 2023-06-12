3cea0212471a4ff40681f6590df23ea336c6d648
isnendyan <isnendyankp@gmail.com>
2023-06-12 10:52:50 +0700

    S 6-121: easy read comment

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -5,8 +5,10 @@ export function Person({ name, nickName = 'shakeAndBake', images }) {
   // if the images exist, then look for the first one
   // if the first one exist, then get me the small property
   // const img = images && images[0] && images [0].small && images [0].small.url;
+
   // shorthand image>first one>small>url
   // const img = images?.[0]?.small?.url;
+  
   // shorthand image>first one>small>url> if not use avatar default
   const img = images?.[0]?.small?.url || avatar;
   return (
