5f936ad33d523e1decc098ebd99d50f4df934187
isnendyan <isnendyankp@gmail.com>
2023-06-12 10:49:22 +0700

    S 6-121: shorthand image>first one>small>url

--- src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export function Person ({ name, nickName = 'shakeAndBake', images }) {
   // if the images exist, then look for the first one
   // if the first one exist, then get me the small property
-  const img = images && images[0] && images [0].small && images [0].small.url;
+  // const img = images && images[0] && images [0].small && images [0].small.url;
+  // shorthand image>first one>small>url
+  const img = images?.[0]?.small?.url;
   return (
     <div>
       <img src={img} alt={name} style={{ width: '50px' }} />
