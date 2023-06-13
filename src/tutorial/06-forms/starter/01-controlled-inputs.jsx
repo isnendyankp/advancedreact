707745eaca7cfdf5eddac8242c780edd5609ad38
isnendyan <isnendyankp@gmail.com>
2023-06-13 11:08:47 +0700

    S 6-122: add form email  with form label & input

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -3,8 +3,16 @@ const ControlledInputs = () => {
     <form className="form">
       <h4>controlled inputs</h4>
       <div className="form-row">
-        <label htmlFor="name" className="form-label">name</label>
-        <input type="text" id="name" className="form-input"/>
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input type="text" id="name" className="form-input" />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input type="email" id="email" className="form-input" />
       </div>
     </form>
   );
