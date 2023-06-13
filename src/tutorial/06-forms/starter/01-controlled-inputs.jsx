62f91828d30c8c9b51277e89aaddd5e7d14a4fcf
isnendyan <isnendyankp@gmail.com>
2023-06-13 11:05:50 +0700

    S 6-122: add form row with form label & input

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -2,6 +2,10 @@ const ControlledInputs = () => {
   return (
     <form className="form">
       <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">name</label>
+        <input type="text" id="name" className="form-input"/>
+      </div>
     </form>
   );
 };
