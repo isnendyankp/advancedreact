928e8096babb95a1eb4c4b9771977b91e1e6cd14
isnendyan <isnendyankp@gmail.com>
2023-06-22 11:17:49 +0700

    S 6-128: cr8 & navigate state with boolean value

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
 const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
+  const [shipping, setShipping] = useState(false);
+
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Other Inputs</h4>
         {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
+        <div className="form-row" style={{ textAlign: 'left' }}>
+          <label htmlFor="shipping"> Free Shipping </label>
+          <input
+            type="checkbox"
+            id="shipping"
+            name="shipping"
+          />
         </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
+        <div className="form-row" style={{ textAlign: 'left' }}>
+          <label htmlFor="framework" className="form-label">
             Framework
           </label>
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
