2934956847d9f9e32220535a10b09e04bf5dd663
isnendyan <isnendyankp@gmail.com>
2023-06-22 11:20:15 +0700

    S 6-128: cr8 handleShipping function

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -3,6 +3,11 @@ const frameworks = ['react', 'angular', 'vue', 'svelte'];
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false);
 
+  const handleShipping = (e) => {
+    console.log(e.target.checked);
+    setShipping(e.target.checked);
+  };
+
   return (
     <div>
       <form className="form">
