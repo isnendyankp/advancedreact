1ba3fa5257ea812d4cc0ae8043afc23198a723ac
isnendyankp <isnendyankp@gmail.com>
2023-06-27 11:19:28 +0700

    S 6-132: add refContainer on input name

--- src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -2,27 +2,33 @@ import { useEffect, useRef, useState } from 'react';
 
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
+  // current property null
   const refContainer = useRef(null);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
   return (
     <div>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type='text' id='name' className='form-input' />
+          <input
+            type="text"
+            id="name"
+            ref={refContainer}
+            className="form-input"
+          />
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       <h1>value : {value}</h1>
-      <button onClick={() => setValue(value + 1)} className='btn'>
+      <button onClick={() => setValue(value + 1)} className="btn">
         increase
       </button>
     </div>
