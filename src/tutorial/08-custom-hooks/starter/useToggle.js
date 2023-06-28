b5c77fd4299d3707242bb40acd6093459258f3c3
isnendyankp <isnendyankp@gmail.com>
2023-06-28 10:35:40 +0700

    S 6-135: cr8 toggle function

--- src/tutorial/08-custom-hooks/starter/useToggle.js
+++ src/tutorial/08-custom-hooks/starter/useToggle.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 
 const useToggle = (defaultValue) => {
   const [show, setShow] = useState(defaultValue);
+  const toggle = () => {
+    setShow(!show);
+  };
 
+  return { show, toggle };
 };
 
+export default useToggle;
