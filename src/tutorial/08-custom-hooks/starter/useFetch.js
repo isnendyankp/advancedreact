a294d5a051b769703ea163c93d04f891324f6f38
isnendyankp <isnendyankp@gmail.com>
2023-06-30 09:51:01 +0700

    S6-137: add & custom useEffect for useFetch.js

--- src/tutorial/08-custom-hooks/starter/useFetch.js
+++ src/tutorial/08-custom-hooks/starter/useFetch.js
@@ -5,3 +5,28 @@ const useFetchPerson = (url) => {
   const [isError, setIsError] = useState(false);
   // change state value
   const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // change name
+    const fetchData = async () => {
+      try {
+        const resp = await fetch(url);
+
+        if (!resp.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          return;
+        }
+        // change to response
+        const response = await resp.json();
+        setData(response);
+      } catch (error) {
+        setIsError(true);
+        // console.log(error);
+      }
+      // hide loading
+      setIsLoading(false);
+    };
+    // invoke fetch data
+    fetchData();
+  }, []);
