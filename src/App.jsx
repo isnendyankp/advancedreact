d0afc36f4d50f29fb8d35225562778f323328e7e
isnendyankp <isnendyankp@gmail.com>
2023-07-03 11:40:36 +0700

    S6-139: Navigation Navbar to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Starter from './tutorial/09-context-api/starter';
+import Final from './tutorial/09-context-api/final/context/Navbar';
+import Starter from './tutorial/09-context-api/starter/Navbar';
 
 function App() {
   return (
     <div className="container">
+      <Final />
       <Starter />
     </div>
   );
