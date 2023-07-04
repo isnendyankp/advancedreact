402f0521b793298cf9b1e7ada07a1cf5c0af9f8d
isnendyankp <isnendyankp@gmail.com>
2023-07-04 12:01:50 +0700

    S6-139: set btn log out on UserContainer

--- src/tutorial/09-context-api/starter/UserContainer.jsx
+++ src/tutorial/09-context-api/starter/UserContainer.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const UserContainer = ({user, logout}) => {
-  return 
+  return (
+    <div className="user-container">
+      <p>Hello There, {user.name}</p>
+      
+      <button className="btn" onClick={logout}>
+        logout
+      </button>
+    </div>
+  );
 }
 
 export default UserContainer
