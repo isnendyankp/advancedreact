80615b2cd6224d4a427ee40562fb835dfe962664
isnendyankp <isnendyankp@gmail.com>
2023-07-04 12:03:24 +0700

    S6-139: set undifined if use not exist

--- src/tutorial/09-context-api/starter/UserContainer.jsx
+++ src/tutorial/09-context-api/starter/UserContainer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const UserContainer = ({user, logout}) => {
   return (
     <div className="user-container">
-      <p>Hello There, {user.name}</p>
-      
+      <p>Hello There, {user?.name}</p>
+
       <button className="btn" onClick={logout}>
         logout
       </button>
