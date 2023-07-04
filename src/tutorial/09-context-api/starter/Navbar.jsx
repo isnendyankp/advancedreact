5c46d720d40eb57a267890f163c29bb6b9f17b7a
isnendyankp <isnendyankp@gmail.com>
2023-07-04 11:46:01 +0700

    S6-139: Cr8 useState

--- src/tutorial/09-context-api/starter/Navbar.jsx
+++ src/tutorial/09-context-api/starter/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import NavLinks from './NavLinks';
 
 const Navbar = () => {
+  // create useState with object name is bob
+  const [user, setUser] = useState({ name: 'bob' });
   return (
     <nav className="navbar">
       <h5>CONTEXT API</h5>
