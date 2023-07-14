a5ef51dd03079c90be721689fe9d64102e4ec2e4
isnendyankp <isnendyankp@gmail.com>
2023-07-14 10:59:29 +0700

    S6-156: move state with variable count & function setCount

--- src/tutorial/11-performance/starter/01-lower-state/Counter.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/Counter.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { useState } from 'react';
 
 const Counter = () => {
-  return (
-    <button
-      className="btn"
-      onClick={() => setCount(count + 1)}
-      style={{ marginBottom: '1rem' }}
-    >
-      count {count}
-    </button>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <button
+        className="btn"
+        onClick={() => setCount(count + 1)}
+        style={{ marginBottom: '1rem' }}
+        >
+        count {count}
+        </button>
+    );
 }
 
 export default Counter
--- src/tutorial/11-performance/starter/01-lower-state/index.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/index.jsx
@@ -3,7 +3,7 @@ import { data } from '../../../../data';
 import List from './List';
 const LowerState = () => {
   const [people, setPeople] = useState(data);
-  const [count, setCount] = useState(0);
+  
 
   return (
     <section>
