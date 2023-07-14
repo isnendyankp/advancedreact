38079960d4d073ba7426576a155429288370058f
isnendyankp <isnendyankp@gmail.com>
2023-07-14 11:00:40 +0700

    S6-156: setup Counter Component

--- src/tutorial/11-performance/starter/01-lower-state/index.jsx
+++ src/tutorial/11-performance/starter/01-lower-state/index.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { data } from '../../../../data';
 import List from './List';
+import Counter from './Counter';
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   
 
   return (
     <section>
-      
+      <Counter />
       <List people={people} />
     </section>
   );
