04ad364a90ce1a883ecbf7bd52122834aeaaafca
isnendyankp <isnendyankp@gmail.com>
2023-07-16 14:07:31 +0700

    S6-156: move form structure at index to form

--- src/tutorial/11-performance/starter/02-lower-state-challenge/Form.jsx
+++ src/tutorial/11-performance/starter/02-lower-state-challenge/Form.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
-  )
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="form-input"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <button className="btn btn-block" type="submit">
+        submit
+      </button>
+    </form>
+  );
 }
 
 export default Form
--- src/tutorial/11-performance/starter/02-lower-state-challenge/index.jsx
+++ src/tutorial/11-performance/starter/02-lower-state-challenge/index.jsx
@@ -21,24 +21,7 @@ const LowerStateChallenge = () => {
   };
   return (
     <section>
-      <form className='form' onSubmit={handleSubmit}>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            className='form-input'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <button className='btn btn-block' type='submit'>
-          submit
-        </button>
-      </form>
+      
       <List people={people} />
     </section>
   );
