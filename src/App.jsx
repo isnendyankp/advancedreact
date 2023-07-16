cca9bd3c3bfd2cf2074259cad2f499a2abf59fdb
isnendyankp <isnendyankp@gmail.com>
2023-07-16 14:02:11 +0700

    S6-156: setup lower state challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Final from './tutorial/11-performance/final/01-lower-state';
+import Final from './tutorial/11-performance/final/02-lower-state-challenge';
 
-import Starter from './tutorial/11-performance/starter/01-lower-state';
+import Starter from './tutorial/11-performance/starter/02-lower-state-challenge';
 
 function App() {
   return (
