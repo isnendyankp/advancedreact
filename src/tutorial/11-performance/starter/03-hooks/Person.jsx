b23aa4e000103c366987b4865ec6f4c597855dc6
isnendyankp <isnendyankp@gmail.com>
2023-07-18 09:32:03 +0700

    S6-159: add button for pass new parameter

--- src/tutorial/11-performance/starter/03-hooks/Person.jsx
+++ src/tutorial/11-performance/starter/03-hooks/Person.jsx
@@ -2,9 +2,11 @@ const Person = ({ name, id, removePerson }) => {
   return (
     <div>
       <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>remove</button>
     </div>
   );
 };
 export default Person;
 
 // add two new parameter : id, removePerson
+// add button with onclick for pass parameter: removePerson(id)
