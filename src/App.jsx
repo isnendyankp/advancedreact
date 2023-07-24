6767b6904f46485f7a3f941ceda10b80b1e7355d
isnendyankp <isnendyankp@gmail.com>
2023-07-24 10:09:52 +0700

    S6-163: setup UseTransition Hook project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Final from './tutorial/11-performance/final/03-hooks';
+import Final from './tutorial/11-performance/final/04-react-18';
 
-import Starter from './tutorial/11-performance/starter/03-hooks';
+import Starter from './tutorial/11-performance/starter/04-react-18';
 
 function App() {
   return (
